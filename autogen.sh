053d8d26f93f413365ec0c30f2dd333ddfe68410
Alexandru IANCU <andu@batracienii.lan>
2016-01-01 15:44:01 +0200

    build script changes

--- autogen.sh
+++ autogen.sh
@@ -7,6 +7,17 @@ test -n "$srcdir" || srcdir=.
 OLDDIR=`pwd`
 cd $srcdir
 
+LIBTOOLIZE=`which libtoolize`
+if test -z $LIBTOOLIZE; then
+    echo "*** No LIBTOOLIZE found, please install it ***"
+    exit 1
+fi
+
+libtoolize
+aclocal
+autoheader
+automake --add-missing
+
 AUTORECONF=`which autoreconf`
 if test -z $AUTORECONF; then
     echo "*** No autoreconf found, please install it ***"
@@ -17,5 +28,5 @@ autoreconf --force --install --verbose
 
 cd $OLDDIR
 #echo "$@"
-test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
+test -n "$NOCONFIGURE" || "$srcdir/rebuild.sh" "$@"
 
