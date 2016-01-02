36083c8365928d159df5a65d7f08a8d93d3c6b35
Alexandru IANCU <alexandru.iancu@gmail.com>
2016-01-02 09:35:13 +0000

    build scripts path are now relative

--- rebuild.sh
+++ rebuild.sh
@@ -2,11 +2,9 @@
 
 autoreconf
 
-#./configure --enable-debug=true --with-csvparser=/home/andu/src/dev/c_c++/libcsv_parser++-1.0.0 --with-libxml=/usr --with-gtest=/home/andu/src/dev/c_c++/git/googletest/googletest CFLAGS="-D__ATTACH_DEBUG__" CXXFLAGS="-D__ATTACH_DEBUG__"
+#./configure --enable-debug=true --with-csvparser=../libcsv_parser-1.0.0/libcsv_parser++-1.0.0 --with-libxml=/usr --with-gtest=../googletest CFLAGS="-D__ATTACH_DEBUG__" CXXFLAGS="-D__ATTACH_DEBUG__"
 
-#./configure --enable-debug=true --with-csvparser=/home/andu/src/dev/c_c++/libcsv_parser++-1.0.0 --with-libxml=/usr --with-gtest=/home/andu/src/dev/c_c++/git/googletest/googletest
-
-./configure --enable-debug=true --with-csvparser=/home/andu/src/src/github_work/libcsv_parser-1.0.0/libcsv_parser++-1.0.0 --with-libxml=/usr --with-gtest=/home/andu/src/src/github_work/googletest/googletest
+./configure --enable-debug=true --with-csvparser=../libcsv_parser-1.0.0/libcsv_parser++-1.0.0 --with-libxml=/usr --with-gtest=../googletest
 
 make VERBOSE=1
 
